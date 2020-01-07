e7331a2be84d86add978aed3065de4545b7029ac
daijiroh-suzuki <daijiroh.suzuki@gmail.com>
2020-01-07 22:00:03 +0900

    定数を整理

--- puyopuyo_test/src/game/puyopuyo/screen/ModeScreen.java
+++ puyopuyo_test/src/game/puyopuyo/screen/ModeScreen.java
@@ -22,6 +22,27 @@ public class ModeScreen extends BaseScreen {
 //	/** ゲームモード：おぷしょん */
 //	private static final int MODE_OPTION   = 3;
 
+	/** モード選択画像：基準座標y */
+	private static final int IMG_MODE_Y = 100;
+	/** モード選択画像：幅 */
+	private static final int IMG_MODE_W = 112;
+	/** モード選択画像：高さ */
+	private static final int IMG_MODE_H = 88;
+
+	/** 右矢印画像：基準座標y */
+	private static final int IMG_ARROWR_Y = 50;
+	/** 右矢印画像：幅 */
+	private static final int IMG_ARROWR_W = 32;
+	/** 右矢印画像：高さ */
+	private static final int IMG_ARROWR_H = 50;
+
+	/** 左矢印画像：基準座標y */
+	private static final int IMG_ARROWL_Y = 0;
+	/** 左矢印画像：幅 */
+	private static final int IMG_ARROWL_W = 32;
+	/** 左矢印画像：高さ */
+	private static final int IMG_ARROWL_H = 50;
+
 	/** コントローラー */
 	private Controller  controller;
 	/** 選択ゲームモード */
@@ -90,43 +111,43 @@ public class ModeScreen extends BaseScreen {
 		g.setColor(Color.WHITE);
 		CommonUtil.drawString("ゲームモードを選択してください", 10, 50, new Font("Meiryo UI", Font.BOLD, 24), g);
 
-		// モードを描画
+		// モードを描画(3倍表示)
 		g.drawImage(ImageManager.modeImage,
-				(MainPanel.WIDTH / 2) - (112 * 3 / 2),
-				(MainPanel.HEIGHT / 2) - (88 * 3 / 2),
-				(MainPanel.WIDTH / 2) - (112 * 3 / 2) + 112 * 3,
-				(MainPanel.HEIGHT / 2) - (88 * 3 / 2) + 88 * 3,
-				select * 112,
-				100,
-				select * 112 + 112,
-				100 + 88,
+				(MainPanel.WIDTH  / 2) - (IMG_MODE_W * 3 / 2),
+				(MainPanel.HEIGHT / 2) - (IMG_MODE_H * 3 / 2),
+				(MainPanel.WIDTH  / 2) - (IMG_MODE_W * 3 / 2) + IMG_MODE_W * 3,
+				(MainPanel.HEIGHT / 2) - (IMG_MODE_H * 3 / 2) + IMG_MODE_H * 3,
+				select * IMG_MODE_W,
+				IMG_MODE_Y,
+				select * IMG_MODE_W + IMG_MODE_W,
+				IMG_MODE_Y + IMG_MODE_H,
 				null);
 
 		if(select != 0) {
-			// 左矢印を描画
+			// 左矢印を描画(3倍表示)
 			g.drawImage(ImageManager.modeImage,
 					30,
 					160,
-					30 + 32 * 3,
-					160 + 50 * 3,
-					(frameCount / 10) % 13 * 32,
-					0,
-					(frameCount / 10) % 13 * 32 + 32,
-					50,
+					30  + IMG_ARROWL_W * 3,
+					160 + IMG_ARROWL_H * 3,
+					(frameCount / 10) % 13 * IMG_ARROWL_W,
+					IMG_ARROWL_Y,
+					(frameCount / 10) % 13 * IMG_ARROWL_W + IMG_ARROWL_W,
+					IMG_ARROWL_Y + IMG_ARROWL_H,
 					null);
 		}
 
 		if(select != MODE_NUM -1) {
-			// 右矢印を描画
+			// 右矢印を描画(3倍表示)
 			g.drawImage(ImageManager.modeImage,
 					515,
 					160,
-					515 + 32 * 3,
-					160 + 50 * 3,
-					(frameCount / 10) % 13 * 32,
-					50,
-					(frameCount / 10) % 13 * 32+ 32,
-					100,
+					515 + IMG_ARROWR_W * 3,
+					160 + IMG_ARROWR_H * 3,
+					(frameCount / 10) % 13 * IMG_ARROWR_W,
+					IMG_ARROWR_Y,
+					(frameCount / 10) % 13 * IMG_ARROWR_W + IMG_ARROWR_W,
+					IMG_ARROWR_Y + IMG_ARROWR_H,
 					null);
 		}
 	}
