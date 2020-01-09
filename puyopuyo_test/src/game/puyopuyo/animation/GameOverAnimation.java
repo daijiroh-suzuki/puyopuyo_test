d8011f406f25a03e2417758d9979597b69ddfde0
daijiroh-suzuki <daijiroh.suzuki@gmail.com>
2020-01-09 21:19:21 +0900

    ステージ画像に合わせてゲームオーバーアニメーション

--- puyopuyo_test/src/game/puyopuyo/animation/GameOverAnimation.java
+++ puyopuyo_test/src/game/puyopuyo/animation/GameOverAnimation.java
@@ -2,7 +2,6 @@ package game.puyopuyo.animation;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.FontMetrics;
 import java.awt.Graphics;
 
 public class GameOverAnimation extends BaseAnimation {
@@ -29,20 +28,15 @@ public class GameOverAnimation extends BaseAnimation {
 	@Override
 	public void draw(Graphics g) {
 		g.setColor(clr);
-		g.fillRect(0, 0, 256, 480);
+		g.fillRect(32, 64, 192, 416);
 
 		// 変更前のフォントを取得
 		Font tmp = g.getFont();
 		// 描画フォントを設定
 		g.setFont(new Font("Meiryo UI", Font.BOLD, 24));
-		// フォント情報を取得
-		FontMetrics fm = g.getFontMetrics();
-		// アニメーション背景を描画
-		g.setColor(Color.BLACK);
-		g.fillRect(0, 240-fm.getHeight(), 256, fm.getHeight()+10);
 		// ゲームオーバー文字列を描画
 		g.setColor(Color.RED);
-		g.drawString("GAME OVER", 60, 240);
+		g.drawString("GAME OVER", 50, 240);
 		// フォントを変更前に戻す
 		g.setFont(tmp);
 	}
