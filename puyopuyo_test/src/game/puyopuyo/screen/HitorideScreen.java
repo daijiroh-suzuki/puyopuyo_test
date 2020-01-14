175846385c65d64a8af4d3fa3f7e262555537b59
daijiroh-suzuki <daijiroh.suzuki@gmail.com>
2020-01-15 01:30:55 +0900

    NPC操作を追加

--- puyopuyo_test/src/game/puyopuyo/screen/HitorideScreen.java
+++ puyopuyo_test/src/game/puyopuyo/screen/HitorideScreen.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import game.puyopuyo.MainPanel;
 import game.puyopuyo.animation.BaseAnimation;
@@ -25,33 +26,11 @@ public class HitorideScreen extends BaseScreen {
 	private Controller controller;
 	/** ステージ */
 	private Stage stage;
-	/** フィールド(1P) */
-	private Field field1;
-	/** フィールド(NPC) */
-	private Field field2;
-	/** NEXTぷよ(1P) */
-	private NextPuyo nextPuyo1;
-	/** NEXTぷよ(NPC) */
-	private NextPuyo nextPuyo2;
-	/** 組ぷよ(1P) */
-	private KumiPuyo kumiPuyo1;
-	/** 組ぷよ(NPC) */
-	private KumiPuyo kumiPuyo2;
-	/** スコア(1P) */
-	private Score score1;
-	/** スコア(NPC) */
-	private Score score2;
-	/** アニメーションリスト(1P) */
-	private List<BaseAnimation> animation1;
-	/** アニメーションリスト(NPC) */
-	private List<BaseAnimation> animation2;
-	/** 処理フェーズ(1P) */
-	private Phase phase1;
-	/** 処理フェーズ(NPC) */
-	private Phase phase2;
+	/** プレイヤーセット(1P) */
+	private PlayerSet player;
+	/** プレイヤーセット(NPC) */
+	private PlayerSet npc;
 
-	/** フレームカウント */
-	private int frameCount = 0;
 	/** 組ぷよが自動落下するフレーム数 */
 	private int autoDropCount = 30;
 
@@ -63,6 +42,26 @@ public class HitorideScreen extends BaseScreen {
 	private GameOverAnimation ga;
 
 	/**
+	 * プレイヤーセット
+	 */
+	private class PlayerSet {
+		/** フィールド */
+		public Field field;
+		/** NEXTぷよ */
+		public NextPuyo nextPuyo;
+		/** 組ぷよ */
+		public KumiPuyo kumiPuyo;
+		/** スコア */
+		public Score score;
+		/** アニメーションリスト */
+		public List<BaseAnimation> animation;
+		/** 処理フェーズ */
+		public Phase phase;
+		/** フレームカウント */
+		public int frameCount = 0;
+	}
+
+	/**
 	 * コンストラクタ
 	 *
 	 * @param controller
@@ -72,24 +71,27 @@ public class HitorideScreen extends BaseScreen {
 		this.controller = controller;
 		// ステージを生成
 		stage = new Stage();
-		// フィールドを生成
-		field1 = new Field(0, 0);
-		field2 = new Field(384, 0);
-		// スコアを生成
-		score1 = new Score(8*Field.TILE_SIZE, 12*Field.TILE_SIZE);
-		score2 = new Score(9*Field.TILE_SIZE, 13*Field.TILE_SIZE);
-		// NEXTぷよを生成
-		nextPuyo1 = new NextPuyo(224, 128, 4);
-		nextPuyo2 = new NextPuyo(320, 128, 4);
-		// アニメーションリストを生成
-		animation1 = new ArrayList<BaseAnimation>();
-		animation2 = new ArrayList<BaseAnimation>();
+
+		// プレイヤーセット(1P)を生成
+		player           = new PlayerSet();
+		player.field     = new Field(0, 0);
+		player.nextPuyo  = new NextPuyo(224, 128, 4);
+		player.score     = new Score(8*Field.TILE_SIZE, 12*Field.TILE_SIZE);
+		player.animation = new ArrayList<BaseAnimation>();
+		player.phase     = Phase.READY;
+
+		// プレイヤーセット(NPC)を生成
+		npc = new PlayerSet();
+		npc.field     = new Field(384, 0);
+		npc.nextPuyo  = new NextPuyo(320, 128, 4);
+		npc.score     = new Score(9*Field.TILE_SIZE, 13*Field.TILE_SIZE);
+		npc.animation = new ArrayList<BaseAnimation>();
+		npc.phase     = Phase.READY;
+
 		// 準備アニメーションを追加
-		animation1.add(new ReadyAnimation());
-		animation2.add(new ReadyAnimation(384, 0));
-		// 処理フェーズを初期化
-		phase1 = Phase.READY;
-		phase2 = Phase.READY;
+		player.animation.add(new ReadyAnimation());
+		npc.animation.add(new ReadyAnimation(384, 0));
+
 		// 一時停止フラグ
 		pause = false;
 	}
@@ -107,7 +109,7 @@ public class HitorideScreen extends BaseScreen {
 				pause = true;
 				// 一時停止アニメーションをリストに追加
 				pa = new PauseAnimation();
-				animation1.add(pa);
+				player.animation.add(pa);
 			} else {
 				// 一時停止フラグOFF
 				pause = false;
@@ -118,26 +120,54 @@ public class HitorideScreen extends BaseScreen {
 		// 一時停止中の場合は処理を中断
 		if(pause) return;
 
-		switch(phase1) {
+		// === プレイヤー ===
+		switch(player.phase) {
 		case READY:
 			// 準備中フェーズ
-			readyPhase();
+			readyPhase(player);
 			break;
 		case CONTROL:
 			// 操作中フェーズ
-			controlPhase();
+			controlPhase(player);
 			break;
 		case DROP:
 			// 落下処理中フェーズ
-			dropPhase();
+			dropPhase(player);
 			break;
 		case VANISH:
 			// 消滅処理中フェーズ
-			vanishPhase();
+			vanishPhase(player);
 			break;
 		case GAMEOVER:
 			// ゲームオーバーフェーズ
-			gameOverPhase();
+			gameOverPhase(player);
+			break;
+		default:
+			// とりあえず難易度選択はなし
+			break;
+		}
+
+		// === NPC ===
+		switch(npc.phase) {
+		case READY:
+			// 準備中フェーズ
+			readyPhase(npc);
+			break;
+		case CONTROL:
+			// 操作中フェーズ
+			controlPhaseNPC(npc);
+			break;
+		case DROP:
+			// 落下処理中フェーズ
+			dropPhase(npc);
+			break;
+		case VANISH:
+			// 消滅処理中フェーズ
+			vanishPhase(npc);
+			break;
+		case GAMEOVER:
+			// ゲームオーバーフェーズ
+			gameOverPhase(npc);
 			break;
 		default:
 			// とりあえず難易度選択はなし
@@ -145,19 +175,9 @@ public class HitorideScreen extends BaseScreen {
 		}
 
 		// アニメーション更新
-		for(int i=0; i<animation1.size(); i++) {
-			BaseAnimation a = animation1.get(i);
-			a.update();
-			if(!a.isRunning()) {
-				if(a instanceof DropAnimation) {
-					// 一時非表示を解除
-					field1.displayTile(((DropAnimation)a).getTo());
-				}
-				// 完了したアニメーションを削除
-				animation1.remove(i);
-				i--;
-			}
-		}
+		updateAnimation(player);
+		updateAnimation(npc);
+
 	}
 
 	/**
@@ -173,80 +193,87 @@ public class HitorideScreen extends BaseScreen {
 		stage.drawBg(g);
 
 		// フィールドを描画
-		field1.draw(g);
-		field2.draw(g);
+		player.field.draw(g);
+
+
+		npc.field.draw(g);
 		// スコアを描画
-		score1.draw(g);
-		score2.draw(g);
+		player.score.draw(g);
+		npc.score.draw(g);
 
 		// NEXTぷよを描画
-		if(nextPuyo1 != null) {
-			nextPuyo1.draw(g);
+		if(player.nextPuyo != null) {
+			player.nextPuyo.draw(g);
 		}
-		if(nextPuyo2 != null) {
-			nextPuyo2.draw(g);
+		if(npc.nextPuyo != null) {
+			npc.nextPuyo.draw(g);
 		}
 
 		// 組ぷよを描画
-		if(kumiPuyo1 != null) {
-			kumiPuyo1.draw(g);
+		if(player.kumiPuyo != null) {
+			player.kumiPuyo.draw(g);
 		}
-		if(kumiPuyo2 != null) {
-			kumiPuyo2.draw(g);
+		if(npc.kumiPuyo != null) {
+			npc.kumiPuyo.draw(g);
 		}
 
 		// ステージ(前景)を描画
 		stage.drawFg(g);
 
 		// アニメーション描画
-		for(BaseAnimation a : animation1) {
+		for(BaseAnimation a : player.animation) {
 			a.draw(g);
 		}
-		for(BaseAnimation a : animation2) {
+		for(BaseAnimation a : npc.animation) {
 			a.draw(g);
 		}
 	}
 
 	/**
 	 * 準備中フェーズ
+	 *
+	 * @param p プレイヤーセット
 	 */
-	private void readyPhase() {
+	private void readyPhase(PlayerSet p) {
 		// 準備中処理が完了したか？
-		if(!isReady(animation1)) {
+		if(!isReady(p.animation)) {
 			// 組ぷよを生成
-			kumiPuyo1 = new KumiPuyo(field1, nextPuyo1.pop());
+			p.kumiPuyo = new KumiPuyo(p.field.getX(), p.field.getY(),
+					p.field, p.nextPuyo.pop());
 			// 処理フェーズを操作中に変更
-			phase1 = Phase.CONTROL;
+			p.phase = Phase.CONTROL;
 		}
 	}
 
 	/**
 	 * 操作中フェーズ
+	 *
+	 * @param p プレイヤーセット
 	 */
-	private void controlPhase() {
+	private void controlPhase(PlayerSet p) {
 
 		// フレームカウントを加算
-		frameCount++;
+		p.frameCount++;
 
 		// 上方向キー押下時（削除予定）
 		if(controller.isKeyUp()) {
 			// 組ぷよを上移動
-			kumiPuyo1.move(KumiPuyo.DIR_UP);
+			p.kumiPuyo.move(KumiPuyo.DIR_UP);
 			controller.setKeyUp(false);
 		}
 		// 下方向キー押下時 or 組ぷよ自動落下時
-		if(controller.isKeyDown() || frameCount >= autoDropCount) {
-			// フレーム貨運ｔおを初期化
-			frameCount = 0;
+		if(controller.isKeyDown() || p.frameCount >= autoDropCount) {
+			// フレームカウントを初期化
+			p.frameCount = 0;
 			// 組ぷよを下移動
-			boolean isFixed = kumiPuyo1.move(KumiPuyo.DIR_DOWN);
+			boolean isFixed = p.kumiPuyo.move(KumiPuyo.DIR_DOWN);
 			if(isFixed) {
 				// 落下処理
-				field1.drop(animation1);
+				p.field.drop(p.animation);
 				// 組ぷよを削除
-				kumiPuyo1 = null;
+				p.kumiPuyo = null;
 				// 処理フェーズを落下処理中に変更
-				phase1 = Phase.DROP;
+				p.phase = Phase.DROP;
 				return;
 			}
 			controller.setKeyDown(false);
@@ -254,88 +281,154 @@ public class HitorideScreen extends BaseScreen {
 		// 右方向キー押下時
 		if(controller.isKeyRight()) {
 			// 組ぷよを右移動
-			kumiPuyo1.move(KumiPuyo.DIR_RIGHT);
+			p.kumiPuyo.move(KumiPuyo.DIR_RIGHT);
 			controller.setKeyRight(false);
 		}
 		// 左方向キー押下時
 		if(controller.isKeyLeft()) {
 			// 組ぷよを左移動
-			kumiPuyo1.move(KumiPuyo.DIR_LEFT);
+			p.kumiPuyo.move(KumiPuyo.DIR_LEFT);
 			controller.setKeyLeft(false);
 		}
 		// Aキー押下時
 		if(controller.isKeyA()) {
 			// 組ぷよを右回転
-			kumiPuyo1.turn(true);
+			p.kumiPuyo.turn(true);
 			controller.setKeyA(false);
 		}
 		// Bキー押下時
 		if(controller.isKeyB()) {
 			// 組ぷよを左回転
-			kumiPuyo1.turn(false);
+			p.kumiPuyo.turn(false);
 			controller.setKeyB(false);
 		}
 	}
 
 	/**
-	 * 落下処理中フェーズ
+	 * 操作中フェーズ
+	 *
+	 * @param p プレイヤーセット
 	 */
-	private void dropPhase() {
+	private void controlPhaseNPC(PlayerSet p) {
+
+		// フレームカウントを加算
+		p.frameCount++;
+
+		// 組ぷよ自動落下
+		if(p.frameCount%autoDropCount == 0) {
+			// 組ぷよを下移動
+			boolean isFixed = p.kumiPuyo.move(KumiPuyo.DIR_DOWN);
+			if(isFixed) {
+				// 落下処理
+				p.field.drop(p.animation);
+				// 組ぷよを削除
+				p.kumiPuyo = null;
+				// 処理フェーズを落下処理中に変更
+				p.phase = Phase.DROP;
+				return;
+			}
+		}
+
+		if(p.frameCount >= 50) {
+			// フレームカウントを初期化
+			p.frameCount = 0;
+			// NPC行動をランダムに取得
+			int action = new Random().nextInt(5);
+			if(action == 0) {
+				// 組ぷよを下移動
+				boolean isFixed = p.kumiPuyo.move(KumiPuyo.DIR_DOWN);
+				if(isFixed) {
+					// 落下処理
+					p.field.drop(p.animation);
+					// 組ぷよを削除
+					p.kumiPuyo = null;
+					// 処理フェーズを落下処理中に変更
+					p.phase = Phase.DROP;
+					return;
+				}
+			} else if(action == 1) {
+				// 組ぷよを右移動
+				p.kumiPuyo.move(KumiPuyo.DIR_RIGHT);
+			} else if(action == 2) {
+				// 組ぷよを左移動
+				p.kumiPuyo.move(KumiPuyo.DIR_LEFT);
+			} else if(action == 3) {
+				// 組ぷよを右回転
+				p.kumiPuyo.turn(true);
+			} else if(action == 4) {
+				// 組ぷよを左回転
+				p.kumiPuyo.turn(false);
+			}
+		}
+
+	}
+
+	/**
+	 * 落下処理中フェーズ
+	 *
+	 * @param p プレイヤーセット
+	 */
+	private void dropPhase(PlayerSet p) {
 		// 落下処理が完了したか？
-		if(!field1.isDrop(animation1)) {
+		if(!p.field.isDrop(p.animation)) {
 			// 消滅処理
-			if(field1.vanish(animation1)) {
+			if(p.field.vanish(p.animation)) {
 				// 消滅対象ありの場合
 				// スコアを更新
-				score1.setScore(field1.getScore());
+				p.score.setScore(p.field.getScore());
 				// 処理フェーズを消滅処理中に変更
-				phase1 = Phase.VANISH;
-			} else if(field1.checkGameOver()) {
+				p.phase = Phase.VANISH;
+			} else if(p.field.checkGameOver()) {
 				// 消滅対象なし & ゲームオーバーマスにぷよが存在する場合
 				// ゲームオーバーアニメーションをリストに追加
-				ga = new GameOverAnimation();
-				animation1.add(ga);
+				ga = new GameOverAnimation(p.field.getX(), p.field.getY());
+				p.animation.add(ga);
 				// 処理フェーズをゲームオーバーに変更
-				phase1 = Phase.GAMEOVER;
+				p.phase = Phase.GAMEOVER;
 			} else {
 				// 消滅対象なしの場合
 				// 新しい組ぷよを生成
-				kumiPuyo1 = new KumiPuyo(field1, nextPuyo1.pop());
+				p.kumiPuyo = new KumiPuyo(p.field.getX(), p.field.getY(),
+						p.field, p.nextPuyo.pop());
 				// 処理フェーズを操作中に変更
-				phase1 = Phase.CONTROL;
+				p.phase = Phase.CONTROL;
 			}
 		}
 	}
 
 	/**
 	 * 消滅処理中フェーズ
+	 *
+	 * @param p プレイヤーセット
 	 */
-	private void vanishPhase() {
+	private void vanishPhase(PlayerSet p) {
 		// 消滅処理が完了したか？
-		if(!field1.isVanish(animation1)) {
+		if(!p.field.isVanish(p.animation)) {
 			// 落下処理
-			field1.drop(animation1);
+			p.field.drop(p.animation);
 			// 処理フェーズを落下処理中に変更
-			phase1 = Phase.DROP;
+			p.phase = Phase.DROP;
 		}
 	}
 
 	/**
 	 * ゲームオーバーフェーズ
+	 *
+	 * @param p プレイヤーセット
 	 */
-	private void gameOverPhase() {
+	private void gameOverPhase(PlayerSet p) {
 		// Startキー押下時
 		if(controller.isKeyStart()) {
 			// ゲームオーバーアニメーションを削除
 			ga.setRunning(false);
 			// フィールドを初期化
-			field1.init();
+			p.field.init();
 			// スコアを初期化
-			score1.init();
+			p.score.init();
 			// 準備アニメーションを追加
-			animation1.add(new ReadyAnimation());
+			p.animation.add(new ReadyAnimation(p.field.getX(), p.field.getY()));
 			// 処理フェーズを操作中に変更
-			phase1 = Phase.READY;
+			p.phase = Phase.READY;
 			controller.setKeyStart(false);
 		}
 	}
@@ -352,5 +445,27 @@ public class HitorideScreen extends BaseScreen {
 			}
 		}
 		return false;
+	}
+
+	/**
+	 * アニメーション更新
+	 *
+	 * @param p プレイヤーセット
+	 */
+	private void updateAnimation(PlayerSet p) {
+
+		for(int i=0; i<p.animation.size(); i++) {
+			BaseAnimation a = p.animation.get(i);
+			a.update();
+			if(!a.isRunning()) {
+				if(a instanceof DropAnimation) {
+					// 一時非表示を解除
+					p.field.displayTile(((DropAnimation)a).getTo());
+				}
+				// 完了したアニメーションを削除
+				p.animation.remove(i);
+				i--;
+			}
+		}
 	}
 }
